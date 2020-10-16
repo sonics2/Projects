86c90f5cfb09f74c8f18b9af359aa87215318de7
sonics2 <72957564+sonics2@users.noreply.github.com>
2020-10-15 23:10:08 -0400

    Update bot.py

--- PythonViewBot/bot.py
+++ PythonViewBot/bot.py
@@ -5,7 +5,7 @@ from selenium import webdriver;
 Timer = 120
 
 #youtube link
-link = 'https://www.youtube.com/watch?v=hW_WFUs3hfQ'
+link = https://www.youtube.com/watch?v=ET0m7ltYLF4&ab_channel=PandaPunch
 
 #number of views
 views = 20
